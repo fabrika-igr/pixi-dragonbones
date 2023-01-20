da652ae315e8542d8afedff90b87352ae37cdc76
Yauhen <yauheng.gamefactory@gmail.com>
2023-01-20 15:55:12 +0300

    clean up

--- DragonBones/src/dragonBones/animation/TimelineState.d.ts
+++
@@ -1,9 +0,0 @@
-import { TimelineState } from "./BaseTimelineState";
-export declare class ActionTimelineState extends TimelineState {
-    static toString(): string;
-    private _onCrossFrame;
-    protected _onArriveAtFrame(): void;
-    protected _onUpdateFrame(): void;
-    update(passedTime: number): void;
-    setCurrentTime(value: number): void;
-}
